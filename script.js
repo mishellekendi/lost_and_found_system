cd924a85f9959fc6dc468e7b07daa3489ae05d45
blessingskioko <blessingskioko@example.com>
2026-08-05 14:40:26 +0300

    Add form validation skeleton

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿function validateForm() {
+    var isValid = true;
+
+    var nameVal = document.getElementById("name").value.trim();
+    var emailVal = document.getElementById("email").value.trim();
+    var catVal = document.getElementById("category").value;
+    var descVal = document.getElementById("description").value.trim();
+    var locVal = document.getElementById("location").value.trim();
+    var dateVal = document.getElementById("date_lost").value.trim();
+
+    return isValid;
+}
