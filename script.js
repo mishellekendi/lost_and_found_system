bce99e34bf68c95f7045cbafd9539199d44b7b5e
blessingskioko <blessingskioko@example.com>
2026-08-05 14:40:27 +0300

    Add name and email validation

--- script.js
+++ script.js
@@ -15,5 +15,18 @@
     document.getElementById("locErr").textContent = "";
     document.getElementById("dateErr").textContent = "";
 
+    if (nameVal === "") {
+        document.getElementById("nameErr").textContent = "Please enter your full name.";
+        isValid = false;
+    }
+
+    if (emailVal === "") {
+        document.getElementById("emailErr").textContent = "Please enter your email.";
+        isValid = false;
+    } else if (emailVal.indexOf("@") === -1 || emailVal.indexOf(".") === -1) {
+        document.getElementById("emailErr").textContent = "Email must contain '@' and '.'.";
+        isValid = false;
+    }
+
     return isValid;
 }
